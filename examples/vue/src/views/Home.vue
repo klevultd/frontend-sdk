a16021206e4c5b496a250cc55d114f5b30cd2f97
jerrypena1 <jerrypena1@hotmail.com>
2022-03-14 09:38:59 -0400

    Add basic navigation and pinia

---
+++ examples/vue/src/views/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
