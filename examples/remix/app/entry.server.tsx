fa1fa9ce79d69a3fa8eabfbeab38eba42788b68e
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2022-03-14 14:06:28 +0200

    Finish up the remix example and create cjs&mjs build

--- examples/remix/app/entry.server.tsx
+++ examples/remix/app/entry.server.tsx
@@ -1,6 +1,12 @@
-import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
-import type { EntryContext } from "remix";
+import { renderToString } from "react-dom/server"
+import { RemixServer } from "remix"
+import type { EntryContext } from "remix"
+import { KlevuConfig } from "@klevu/core"
+
+KlevuConfig.init({
+  url: "https://eucs23v2.ksearchnet.com/cs/v2/search",
+  apiKey: "klevu-160320037354512854",
+})
 
 export default function handleRequest(
   request: Request,
@@ -10,12 +16,12 @@ export default function handleRequest(
 ) {
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
-  );
+  )
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set("Content-Type", "text/html")
 
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
-  });
+  })
 }
