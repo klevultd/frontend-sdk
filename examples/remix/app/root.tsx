beb78cb6f3abd05ef6237014eb656bfc76944628
jerrypena1 <jerrypena1@hotmail.com>
2022-03-17 08:46:24 -0400

    adding category support for vue example

--- examples/remix/app/root.tsx
+++ examples/remix/app/root.tsx
@@ -9,7 +9,7 @@ import {
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Klevu SDK + Remix Example" };
 };
 
 export default function App() {
