7766ee9b2938bf159697ec5d2dd21150df2feeeb
jerrypena1 <jerrypena1@hotmail.com>
2022-04-05 03:10:42 -0400

    Vue Documentation and Updates

--- examples/vue/src/views/Search.vue
+++ examples/vue/src/views/Search.vue
@@ -1,9 +1,180 @@
 <script setup>
+import { ref } from 'vue'
+import { onBeforeRouteUpdate } from 'vue-router'
+import useSearch from '../state/searchStore';
+import Product from '../components/Product.vue';
+import Option from '../components/Option.vue';
+import Slider from '../components/Slider.vue';
+import FacetToggle from '../components/FacetToggle.vue';
+import {
+    applyFilterWithManager,
+    FilterManager,
+    KlevuDomEvents,
+    KlevuFetch,
+    KlevuSearchSorting,
+    listFilters,
+    search,
+    sendSearchEvent,
+} from "@klevu/core";
+
 const props = defineProps({
     searchTerm: String
 })
+const searchStore = useSearch();
+const manager = new FilterManager();
+let prevRes
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+searchStore.resetSearch()
+
+const openFacets = ref(vw >= 1024 ? true : false);
+
+const initialFetch = async () => {
+    const res = await KlevuFetch(
+        search(
+            props.searchTerm,
+            {
+                id: "search",
+                limit: 36,
+                sort: searchStore.sorting,
+            },
+            listFilters({
+                rangeFilterSettings: [
+                    {
+                        key: "klevu_price",
+                        minMax: true,
+                    },
+                ],
+                exclude: searchStore.homeFilterExcludes,
+                filterManager: manager,
+            }),
+            applyFilterWithManager(manager),
+            sendSearchEvent()
+        )
+    )
+    prevRes = res
+
+    const searchResult = res.queriesById('search')
+    if (!searchResult) {
+        return
+    }
+
+    searchStore.showMore = Boolean(res.next)
+    searchStore.setOptions(manager.options)
+    searchStore.setSliders(manager.sliders)
+    searchStore.setProducts(searchResult.records ?? [])
+
+}
+
+const fetchMore = async () => {
+    const nextRes = await prevRes.next({
+        filterManager: manager,
+    })
+    searchStore.setProducts([
+        ...searchStore.products,
+        ...(nextRes.queriesById('search').records ?? []),
+    ])
+    prevRes = nextRes
+    searchStore.showMore = Boolean(nextRes.next)
+}
+
+document.addEventListener(
+    KlevuDomEvents.FilterSelectionUpdate,
+    initialFetch
+)
+
+onBeforeRouteUpdate((to, from) => {
+    document.removeEventListener(
+        KlevuDomEvents.FilterSelectionUpdate,
+        initialFetch
+    )
+})
+
+const toggleFacets = () => {
+    openFacets.value = !openFacets.value
+}
+
+const updateSort = e => {
+    searchStore.sorting = e.target.value
+    initialFetch()
+}
+
+initialFetch()
+
 </script>
 
 <template>
-    <div>this is the search page and the search was {{ searchTerm }}</div>
+    <div class="loading-message" v-show="!searchStore.products.length">Loading products...</div>
+    <div class="flex justify-center items-center h-20">
+        <h3 class="text-2xl text-gray-700 italic">
+            <span class="text-gray-400">Searching:</span>
+            {{ searchTerm }}
+        </h3>
+    </div>
+    <div class="homepage-wrapper" v-show="searchStore.products.length">
+        <section class="filter-section">
+            <div class="facets border p-6">
+                <FacetToggle :handler="toggleFacets" :open="openFacets" :vw="vw" />
+                <div v-show="openFacets">
+                    <Option
+                        v-for="(option, index) in searchStore.options"
+                        :key="index"
+                        :option="option"
+                        :manager="manager"
+                    />
+                    <Slider
+                        v-for="(slider, index) in searchStore.sliders"
+                        :key="index"
+                        :slider="slider"
+                        :manager="manager"
+                    />
+                </div>
+            </div>
+        </section>
+        <section class="results-section">
+            <div class="sorting-options">
+                <select class="border py-2 px-3" v-model="searchStore.sorting" @change="updateSort">
+                    <option :value="KlevuSearchSorting.Relevance">Relevance</option>
+                    <option :value="KlevuSearchSorting.PriceAsc">Price: Low to high</option>
+                    <option :value="KlevuSearchSorting.PriceDesc">Price: High to low</option>
+                </select>
+            </div>
+            <div class="product-results">
+                <Product
+                    v-for="product in searchStore.products"
+                    :key="product.id"
+                    :product="product"
+                    classes="p-2 md:w-1/3 lg:w-1/4 mb-5"
+                />
+                <div class="w-full" v-if="searchStore.showMore">
+                    <button class="btn" @click="fetchMore">Load more</button>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
+
+<style scoped>
+.homepage-wrapper {
+    max-width: 1200px;
+    @apply flex flex-col lg:flex-row mx-auto mt-6 mb-12;
+}
+.filter-section {
+    @apply px-5 lg:w-1/4 mt-12 w-96 mx-auto;
+}
+.facets {
+    max-height: 1000px;
+    overflow-y: auto;
+    overflow-x: hidden;
+}
+.results-section {
+    @apply mt-12 lg:mt-0 lg:w-3/4;
+}
+.sorting-options {
+    @apply px-6 text-center lg:text-left pb-6 lg:pb-0;
+}
+.product-results {
+    max-width: 1000px;
+    @apply md:flex md:flex-wrap mx-auto;
+}
+</style>
