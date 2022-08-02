305e40fa3c084a3d13fd82c71d05d3adcc9b83bb
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2022-08-02 10:43:12 +0300

    Fix react example build

--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -10,6 +10,7 @@ import { CategoryPage } from "./routes/CategoryPage"
 import { SearchResultPage } from "./routes/SearchResultPage"
 import { CheckoutPage } from "./routes/CheckoutPage"
 import axios from "axios"
+import "swiper/css/bundle"
 
 KlevuConfig.init({
   url: "https://eucs29v2.ksearchnet.com/cs/v2/search",
