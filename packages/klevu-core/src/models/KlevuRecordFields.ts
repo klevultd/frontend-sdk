fb9afc6d23bc0a09cf127e0ebc1ee7a7feca5d86
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2022-10-20 15:05:10 +0300

    Remove typefest as dependency

--- packages/klevu-core/src/models/KlevuRecordFields.ts
+++ packages/klevu-core/src/models/KlevuRecordFields.ts
@@ -1,4 +1,4 @@
-import { LiteralUnion } from "type-fest"
+import type { LiteralUnion } from "../utils/literalUnion.js"
 import { KlevuRecord } from "./KlevuRecord.js"
 
 export type KlevuRecordFields = LiteralUnion<keyof KlevuRecord, string>
