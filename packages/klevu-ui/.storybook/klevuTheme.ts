4b42abecfb4897040f897d26886d9273e2294209
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2022-12-07 16:58:46 +0200

    small tunings

--- packages/klevu-ui/.storybook/klevuTheme.ts
+++ packages/klevu-ui/.storybook/klevuTheme.ts
@@ -6,7 +6,9 @@ export default create({
   colorPrimary: "#97C73E",
   colorSecondary: "#97C73E",
 
-  fontBase: "'Poppins', sans-serif",
+  // Typography
+  fontBase: "'Poppins', sans-serif !important",
+  fontCode: "'Source Code Pro', monospace",
 
   /*
   // UI
@@ -15,9 +17,6 @@ export default create({
   appBorderColor: "grey",
   appBorderRadius: 4,
 
-  // Typography
-  fontCode: "monospace",
-
   // Text colors
   textColor: "black",
   textInverseColor: "white",
@@ -25,8 +24,8 @@ export default create({
 
   // Toolbar default and active colors
   barTextColor: "#eee",
-  barSelectedColor: "#333",
-  barBg: "#97C73E",
+  barSelectedColor: "#e5e5e5",
+  barBg: "#0A4563",
 
   /*
   // Form colors
