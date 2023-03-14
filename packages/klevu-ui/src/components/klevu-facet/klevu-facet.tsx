63b02d1fc666568bad9eb204b5982f0fdf180904
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2023-03-14 17:09:07 +0200

    Fix web component facet list and vanilla example

--- packages/klevu-ui/src/components/klevu-facet/klevu-facet.tsx
+++ packages/klevu-ui/src/components/klevu-facet/klevu-facet.tsx
@@ -146,7 +146,6 @@ export class KlevuFacet {
 
     let showAllButton = false
     if (!this.showAll && opts.length > 5) {
-      console.log(opts.length)
       opts = opts.slice(0, 5)
       showAllButton = true
     }
@@ -162,45 +161,47 @@ export class KlevuFacet {
           {this.option.label}
         </klevu-typography>
 
-        {opts.map((o) => (
-          <div class="option">
-            {this.mode === "checkbox" ? (
-              <klevu-checkbox
-                exportparts={globalExportedParts}
-                checked={o.selected}
-                name={this.option!.key}
-                onKlevuCheckboxChange={(event: CustomEvent<boolean>) => {
-                  this.manager.toggleOption(this.option!.key, o.name)
-                }}
-              >
-                <div class="container">
-                  <span class="name">
-                    <span>{o.name}</span>
-                  </span>
-                  <span class="count">({o.count})</span>
-                </div>
-              </klevu-checkbox>
-            ) : (
-              <div class="container">
-                <input
-                  type="radio"
-                  id={this.option!.key}
-                  name={this.option!.key}
-                  value={o.value}
+        <div class="options" slot="content">
+          {opts.map((o) => (
+            <div class="option">
+              {this.mode === "checkbox" ? (
+                <klevu-checkbox
+                  exportparts={globalExportedParts}
                   checked={o.selected}
-                  onClick={() => {
-                    this.manager.clearOptionSelections(this.option!.key)
+                  name={this.option!.key}
+                  onKlevuCheckboxChange={(event: CustomEvent<boolean>) => {
                     this.manager.toggleOption(this.option!.key, o.name)
                   }}
-                />
-                <label htmlFor={this.option!.key} class="name">
-                  <span>{o.name}</span>
-                </label>
-                <span class="count">({o.count})</span>
-              </div>
-            )}
-          </div>
-        ))}
+                >
+                  <div class="container">
+                    <span class="name">
+                      <span>{o.name}</span>
+                    </span>
+                    <span class="count">({o.count})</span>
+                  </div>
+                </klevu-checkbox>
+              ) : (
+                <div class="container">
+                  <input
+                    type="radio"
+                    id={this.option!.key}
+                    name={this.option!.key}
+                    value={o.value}
+                    checked={o.selected}
+                    onClick={() => {
+                      this.manager.clearOptionSelections(this.option!.key)
+                      this.manager.toggleOption(this.option!.key, o.name)
+                    }}
+                  />
+                  <label htmlFor={this.option!.key} class="name">
+                    <span>{o.name}</span>
+                  </label>
+                  <span class="count">({o.count})</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
         {showAllButton ? (
           <klevu-button
             style={{ "--klevu-button-text-align": "left" }}
