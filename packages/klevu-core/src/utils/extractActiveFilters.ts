5d8ff6ac64ff8ed7e6c3cad0df714fac6922d1c0
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2023-04-06 08:59:37 +0300

    Create tests and refactor activeFiltersExtract

--- packages/klevu-core/src/utils/extractActiveFilters.ts
+++ packages/klevu-core/src/utils/extractActiveFilters.ts
@@ -9,35 +9,34 @@ export function extractActiveFilters(
 ): string | undefined {
   let selectedFiltersStr = ""
   let isAnyFilterSelected = false
+
   if (!result.filters || result.filters.length === 0) {
     return undefined
   }
-  result.filters?.forEach(function (filter) {
+  result.filters?.forEach((filter) => {
     if (filter.type === KlevuFilterType.Slider) {
-      if (
-        filter.start !== null &&
-        typeof filter.start !== "undefined" &&
-        filter.end !== null &&
-        typeof filter.end !== "undefined"
-      ) {
-        if (filter.start != filter.min || filter.end != filter.max) {
-          if (isAnyFilterSelected) {
-            selectedFiltersStr += ";;"
-          }
-          isAnyFilterSelected = true
-          selectedFiltersStr +=
-            filter.key + ":" + filter.start + " - " + filter.end
+      if (!filter.start || !filter.end) {
+        return
+      }
+      if (filter.start != filter.min || filter.end != filter.max) {
+        if (isAnyFilterSelected) {
+          selectedFiltersStr += ";;"
         }
+        isAnyFilterSelected = true
+        selectedFiltersStr +=
+          filter.key + ":" + filter.start + " - " + filter.end
       }
     } else {
-      filter.options.forEach(function (option) {
-        if (option.selected) {
-          if (isAnyFilterSelected) {
-            selectedFiltersStr += ";;"
-          }
-          isAnyFilterSelected = true
-          selectedFiltersStr += filter.key + ":" + option.name
+      filter.options.forEach((option) => {
+        if (!option.selected) {
+          return
         }
+
+        if (isAnyFilterSelected) {
+          selectedFiltersStr += ";;"
+        }
+        isAnyFilterSelected = true
+        selectedFiltersStr += filter.key + ":" + option.name
       })
     }
   })
