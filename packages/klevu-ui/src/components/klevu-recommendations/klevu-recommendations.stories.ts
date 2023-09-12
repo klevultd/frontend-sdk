02721902de6f5c86320a8ba2896723a6f13ab1a8
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2023-09-12 11:18:42 +0300

    UI: Fix scrolling of recommendation products
    And created a customized recommendation example.

--- packages/klevu-ui/src/components/klevu-recommendations/klevu-recommendations.stories.ts
+++ packages/klevu-ui/src/components/klevu-recommendations/klevu-recommendations.stories.ts
@@ -28,3 +28,34 @@ export const Recommendations: StoryObj<KlevuRecommendations> = {
     recommendation-title=${ifDefined(args.recommendationTitle)}
   ></klevu-recommendations>`,
 }
+
+export const Customized: StoryObj<KlevuRecommendations> = {
+  args: {
+    recommendationId: "k-b1c018f7-ee85-45c0-b65f-b9556f7dc15d",
+  },
+  render: (args) => html`<klevu-recommendations
+      id="customized"
+      recommendation-id=${ifDefined(args.recommendationId)}
+      recommendation-title="A custom recommendation"
+    >
+      <klevu-slides heading="A custom recommendation"></klevu-slides>
+    </klevu-recommendations>
+    <script>
+      const recommendation = document.getElementById("customized")
+      const slides = recommendation.querySelector("#customized klevu-slides")
+      recommendation.addEventListener("klevuData", (e) => {
+        slides.innerHTML = ""
+        e.detail.records.forEach((item) => {
+          const product = document.createElement("klevu-product")
+          product.product = item
+          product.isWrapper = true
+          slides.appendChild(product)
+
+          const productDiv = document.createElement("div")
+          productDiv.style = "width: 200px; border: 1px solid red; padding: 10px; cursor: pointer;"
+          productDiv.innerHTML = item.name
+          product.appendChild(productDiv)
+        })
+      })
+    </script> `,
+}
