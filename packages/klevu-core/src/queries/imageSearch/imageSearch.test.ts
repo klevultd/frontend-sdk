9a27ae05b9aaf4d6deaa248a85a5d8c94ea1e8cc
nikhiln <nikhil.narayan@klevu.com>
2023-09-13 11:06:05 +0530

    added test for calling KlevuFetch

--- packages/klevu-core/src/queries/imageSearch/imageSearch.test.ts
+++ packages/klevu-core/src/queries/imageSearch/imageSearch.test.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { KlevuConfig, KlevuTypeOfRecord, imageSearch } from "../../index.js"
+import {
+  KlevuConfig,
+  KlevuFetch,
+  KlevuTypeOfRecord,
+  imageSearch,
+} from "../../index.js"
 import axios from "axios"
 
 beforeEach(() => {
@@ -51,6 +56,33 @@ test("Image search with blob as param", async () => {
   ).toContain(
     "klevu-image-search-dev.s3.eu-west-1.amazonaws.com/klevu-160320037354512854/image-search"
   )
+})
+
+test("Image search to check results from search api", async () => {
+  const response = await fetch("https://picsum.photos/600/600")
+  const payload = await imageSearch(await response.blob(), {
+    id: "test",
+  })
+  expect(payload.queries).toBeDefined()
+  expect(payload.queries?.length).toBe(1)
+  expect(payload.queries![0].settings?.context?.sourceObjects?.length).toBe(1)
+  expect(
+    payload.queries![0].settings?.context?.sourceObjects![0].typeOfRecord
+  ).toBe(KlevuTypeOfRecord.Image)
+  expect(
+    payload.queries![0].settings?.context?.sourceObjects![0].records.length
+  ).toBe(1)
+  expect(
+    (
+      payload.queries![0].settings?.context?.sourceObjects![0].records![0] as {
+        url: string
+      }
+    ).url
+  ).toContain(
+    "klevu-image-search-dev.s3.eu-west-1.amazonaws.com/klevu-160320037354512854/image-search"
+  )
+  const result = await KlevuFetch(payload)
+  expect(result.queriesById("test")).toBeDefined()
 })
 
 test("Error thrown when no image is passed", async () => {
