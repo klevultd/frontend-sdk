4c8b4b9eab9f337adcf359fe3ba5a24365f5a55d
nikhiln <nikhil.narayan@klevu.com>
2024-07-24 16:18:52 +0530

    correct the values mapped for events url in demo

--- examples/react-klevu-ui/src/app.tsx
+++ examples/react-klevu-ui/src/app.tsx
@@ -71,12 +71,12 @@ export function App() {
           ? JSON.parse(localStorage.getItem("demo-config"))?.url
           : "https://eucs29v2.ksearchnet.com/cs/v2/search"
       }
-      eventsApiV1Url={
+      eventsV1Url={
         localStorage.getItem("demo-config")
           ? JSON.parse(localStorage.getItem("demo-config"))?.eventsApiV1Url
           : ""
       }
-      eventsApiV2Url={
+      eventsV2Url={
         localStorage.getItem("demo-config")
           ? JSON.parse(localStorage.getItem("demo-config"))?.eventsApiV2Url
           : ""
