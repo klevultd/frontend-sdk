d18afaa1ef6e7eb03661fe047b02739d5a1c0161
nikhiln <nikhil.narayan@klevu.com>
2024-11-19 18:00:11 +0530

    added logic to always keep recent clicks for cat pages

--- examples/react/src/routes/CategoryPage.tsx
+++ examples/react/src/routes/CategoryPage.tsx
@@ -4,15 +4,49 @@ import { useState } from "react"
 import { Category } from "../components/category"
 
 export function CategoryPage() {
-  const [showPersonalsation, setShowPersonalsation] = useState(false)
+  const [showPersonalisation, setShowPersonalisation] = useState(false)
+  const handlePersonalisationToggle = (val: boolean) => {
+    if (val) {
+      const lastClicks = localStorage.getItem("klevu-last-clicks")
+      const lastCatClicks = localStorage.getItem("klevu-last-clicks-cat")
+      if (lastClicks) {
+        const parsedClicks = JSON.parse(lastClicks)
+        const itemsToTake = Math.floor(parsedClicks.length / 3) * 3
+        const ids = Array.from(parsedClicks)
+          .reverse()
+          .filter(
+            (item, index, self) =>
+              index === self.findIndex((t) => t.id === item.id)
+          )
+          .slice(0, itemsToTake)
+          .map((i) => i.id)
+        let parsedCatClicks
+        if (lastCatClicks) {
+          parsedCatClicks = JSON.parse(lastCatClicks)
+          parsedCatClicks.ids = ids
+          parsedCatClicks.cached = new Date()
+        } else {
+          parsedCatClicks = {
+            ids,
+            cached: new Date(),
+          }
+        }
+        localStorage.setItem(
+          "klevu-last-clicks-cat",
+          JSON.stringify(parsedCatClicks)
+        )
+      }
+    }
+    setShowPersonalisation(val)
+  }
   return (
     <div>
       <FormControlLabel
         control={
           <Switch
             color="info"
-            checked={showPersonalsation}
-            onChange={(e) => setShowPersonalsation(e.target.checked)}
+            checked={showPersonalisation}
+            onChange={(e) => handlePersonalisationToggle(e.target.checked)}
           />
         }
         label="Show Personalised Section"
@@ -35,7 +69,7 @@ export function CategoryPage() {
           <h3>&nbsp;</h3>
           <Category type="" />
         </div>
-        {showPersonalsation && (
+        {showPersonalisation && (
           <div
             style={{
               width: "100%",
